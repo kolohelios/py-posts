b596fb7c827318c8ed416cc12bf0763ca9dc4ae9
Jon Edwards <jkedwards@me.com>
2016-07-04 01:48:27 +0000

    initial commit

---
+++ tests/api_tests.py
@@ -0,0 +1,31 @@
+import unittest
+import os
+import json
+try: from urllib.parse import urlparse
+except ImportError: from urlparse import urlparse # Python 2 compatibility
+
+# Configure our app to use the testing databse
+os.environ["CONFIG_PATH"] = "posts.config.TestingConfig"
+
+from posts import app
+from posts import models
+from posts.database import Base, engine, session
+
+class TestAPI(unittest.TestCase):
+    """ Tests for the posts API """
+
+    def setUp(self):
+        """ Test setup """
+        self.client = app.test_client()
+
+        # Set up the tables in the database
+        Base.metadata.create_all(engine)
+
+    def tearDown(self):
+        """ Test teardown """
+        session.close()
+        # Remove the tables and their data from the database
+        Base.metadata.drop_all(engine)
+
+if __name__ == "__main__":
+    unittest.main()
